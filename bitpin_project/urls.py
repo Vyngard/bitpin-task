25b644737ecf04f8406719a0bc130178276f7b43
Pirooz Ekhtiyari <pirooz1371@gmail.com>
2024-11-22 12:56:30 -0800

    add views_simple_average and views_dynamic_alpha to urls

--- bitpin_project/urls.py
+++ bitpin_project/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import PostListView, RatingCreateUpdateView, PostCreateView, RatingCreateUpdateViewSimpleAverage, PostResetView
+from .views import PostListView, RatingCreateUpdateView, PostCreateView, PostResetView
+from .views_dynamic_alpha import RatingCreateUpdateViewDynamicAlpha
+from .views_simple_average import RatingCreateUpdateViewSimpleAverage
 
 urlpatterns = [
     path('posts/', PostListView.as_view(), name='post-list'),
@@ -7,4 +9,7 @@ urlpatterns = [
     path('rate/', RatingCreateUpdateView.as_view(), name='rate-post'),
     path('rate_simple/', RatingCreateUpdateViewSimpleAverage.as_view(), name='rate-post-simple'),
     path('reset_post/', PostResetView.as_view(), name='reset_post'),
+    path('rate_dynamic/', RatingCreateUpdateViewDynamicAlpha.as_view(), name='rate-post-dynamic'),
 ]
+
+# TODO: add views_simple_average.py and views_dynamic_alpha.py 
